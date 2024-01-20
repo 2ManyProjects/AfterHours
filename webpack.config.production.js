e82ae322211b127583248348a6d6e20fb693d905
2ManyProjects <shaivkamat@gmail.com>
2024-01-19 23:00:31 -0800

    added build cmd

---
+++ webpack.config.production.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+
+module.exports = {
+    mode: 'production',
+  entry: './src/index.js',
+  experiments: { topLevelAwait: true },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './public/index.html',
+    favicon: './public/favicon.ico',
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 4000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ico', '.png', '.json', '.css'],  
+  },
+};
